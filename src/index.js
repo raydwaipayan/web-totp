2d2dbc026aa01b29059506c17daffefdffdda81e
Dwaipayan Ray <dwaipayanray1@gmail.com>
2021-07-02 01:10:39 +0530

    Add service worker
    Signed-off-by: Dwaipayan Ray <dwaipayanray1@gmail.com>

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { ThemeProvider } from '@material-ui/core/styles';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import theme from './theme/theme';
 
@@ -14,6 +15,11 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root'),
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
