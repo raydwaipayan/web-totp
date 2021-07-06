e90a68793cb7596fcfe77404ca76db3e3df7e63c
Dwaipayan Ray <dwaipayanray1@gmail.com>
2021-07-06 20:43:09 +0530

    Make totp cells dynamically from data
    Signed-off-by: Dwaipayan Ray <dwaipayanray1@gmail.com>

--- src/pages/landing/landing.js
+++ src/pages/landing/landing.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
-import { Container } from '@material-ui/core';
+import { Container, Box } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import TotpCell from '../../components/totpcell/totpcell';
 
@@ -13,16 +13,46 @@ const useStyles = makeStyles((theme) => ({
     borderRadius: '5px',
     padding: theme.spacing(2),
   },
+  cellBox: {
+    borderRadius: '4px',
+    border: '1px solid #CCCCCC',
+  },
+  cell: {
+    borderBottom: '1px solid #CCCCCC',
+  },
 }));
 
 export default function Landing() {
   const classes = useStyles();
+
+  const data = [
+    {
+      secret: 'GEZDGNBVGY3TQOJQ',
+      id: '1',
+      name: 'TestKey',
+    },
+  ];
+
   return (
     <>
       <Container className={classes.root}>
         <Grid container alignItems="center" justify="center">
           <Grid container item xs={12} md={6} className={classes.cellContainer}>
-            <TotpCell secret="GEZDGNBVGY3TQOJQ" id="1" name="Testing" />
+            <Box className={classes.cellBox} width={1} py={2}>
+              {data.map((item, index) => (
+                index === 1
+                  ? (
+                    <Box className={classes.cell} width={1} py={1}>
+                      <TotpCell secret={item.secret} id={item.id} name={item.name} />
+                    </Box>
+                  )
+                  : (
+                    <Box width={1} py={1}>
+                      <TotpCell secret={item.secret} id={item.id} name={item.name} />
+                    </Box>
+                  )
+              ))}
+            </Box>
           </Grid>
         </Grid>
       </Container>
